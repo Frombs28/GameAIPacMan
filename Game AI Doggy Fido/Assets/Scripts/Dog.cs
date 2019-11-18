cf42f70c0fc6c76b7da5968af7522a0f43ffbbb7
Palmer Stolly <palmerstolly@gmail.com>
2019-11-18 16:00:12 -0500

    Changed float name

--- Game AI Doggy Fido/Assets/Scripts/Dog.cs
+++ Game AI Doggy Fido/Assets/Scripts/Dog.cs
@@ -10,18 +10,19 @@ public class Dog : MonoBehaviour {
         energy = 0.8f,
         happiness = 0.4f,
         sleepiness = 0.1f,
-        loyalty = 0.5f,
-        boredom = 0.5f,
+        loyalty = 0.8f,
+        loneliness = 0.2f,
         bathroom = 0.8f;
 
 
     void Update() {
+        //Don't Let Values Exceed 1 or go below 0
         happiness = Mathf.Clamp(happiness, 0, 1);
         sleepiness = Mathf.Clamp(sleepiness, 0, 1);
         energy = Mathf.Clamp(energy, 0, 1);
         hunger = Mathf.Clamp(hunger, 0, 1);
         loyalty = Mathf.Clamp(loyalty, 0, 1);
-        boredom = Mathf.Clamp(boredom, 0, 1);
+        loneliness = Mathf.Clamp(loneliness, 0, 1);
         bathroom = Mathf.Clamp(bathroom, 0, 1);
     }
 
@@ -30,8 +31,8 @@ public class Dog : MonoBehaviour {
         energy = 0.8f;
         happiness = 0.4f;
         sleepiness = 0.1f;
-        loyalty = 0.5f;
-        boredom = 0.5f;
+        loyalty = 0.8f;
+        loneliness = 0.2f;
         bathroom = 0.8f;
     }
 }
